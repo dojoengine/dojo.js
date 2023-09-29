e41755df998414abf367d890dddad303c5a5c6f7
democritus <ponderingdemocritus@protonmail.com>
2023-09-29 11:31:37 +0200

    improve structure of tsup

--- packages/create-burner/tsup.config.ts
+++ packages/create-burner/tsup.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "tsup";
+import { tsupConfig } from '../../tsup.config'
 
 export default defineConfig({
-    entry: ["src/index.ts"],
-    target: "esnext",
-    format: ["esm"],
-    dts: false,
-    sourcemap: true,
-    clean: true,
-    minify: true,
+    ...tsupConfig
 });
--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "tsup";
+import { tsupConfig } from '../../tsup.config'
 
 export default defineConfig({
-    entry: ["src/index.ts"],
-    target: "esnext",
-    format: ["esm"],
-    dts: true,
-    sourcemap: true,
-    clean: true,
-    minify: true,
+    ...tsupConfig
 });
