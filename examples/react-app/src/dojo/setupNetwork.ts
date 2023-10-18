c559dee82e4c16805aeb0a669e85b3439284f13a
Kariy <ammr.arf@gmail.com>
2023-10-19 01:27:48 +0900

    intergrate with torii wasm

--- examples/react-app/src/dojo/setupNetwork.ts
+++ examples/react-app/src/dojo/setupNetwork.ts
@@ -1,44 +1,83 @@
 import { defineContractComponents } from "./contractComponents";
 import { world } from "./world";
-import { RPCProvider, Query, } from "@dojoengine/core";
+import { RPCProvider, Query } from "@dojoengine/core";
 import { Account, num } from "starknet";
-import { GraphQLClient } from 'graphql-request';
-import { getSdk } from '../generated/graphql';
+import { GraphQLClient } from "graphql-request";
+import { getSdk } from "../generated/graphql";
 import manifest from "../../../dojo-starter/target/dev/manifest.json";
+import * as torii from "@dojoengine/torii";
 
 export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
 
 export async function setupNetwork() {
-    // Extract environment variables for better readability.
-    const { VITE_PUBLIC_WORLD_ADDRESS, VITE_PUBLIC_NODE_URL, VITE_PUBLIC_TORII } = import.meta.env;
+	// Extract environment variables for better readability.
+	const { VITE_PUBLIC_WORLD_ADDRESS, VITE_PUBLIC_NODE_URL, VITE_PUBLIC_TORII } =
+		import.meta.env;
 
-    // Create a new RPCProvider instance.
-    const provider = new RPCProvider(VITE_PUBLIC_WORLD_ADDRESS, manifest, VITE_PUBLIC_NODE_URL);
+	// Create a new RPCProvider instance.
+	const provider = new RPCProvider(
+		VITE_PUBLIC_WORLD_ADDRESS,
+		manifest,
+		VITE_PUBLIC_NODE_URL
+	);
 
-    // Return the setup object.
-    return {
-        provider,
-        world,
+	console.log("ohayo");
 
-        // Define contract components for the world.
-        contractComponents: defineContractComponents(world),
+	// // @ts-ignore
+	const client = await torii.spawn_client(
+		"http://0.0.0.0:8080/grpc",
+		"http://0.0.0.0:5050",
+		"0x1af130f7b9027f3748c1e3b10ca4a82ac836a30ac4f2f84025e83a99a922a0c",
+		[
+			{
+				model: "Position",
+				keys: [
+					"0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973",
+				],
+			},
+		]
+	);
 
-        // Define the graph SDK instance.
-        graphSdk: () => getSdk(new GraphQLClient(VITE_PUBLIC_TORII)),
+	setInterval(() => {
+		console.log(
+			"position",
+			client.getModelValue("Position", [
+				"0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973",
+			])
+		);
+	}, 2000);
 
-        // Execute function.
-        execute: async (signer: Account, contract: string, system: string, call_data: num.BigNumberish[]) => {
-            return provider.execute(signer, contract, system, call_data);
-        },
+	// torii.greet();
 
-        // Entity query function.
-        entity: async (component: string, query: Query) => {
-            return provider.entity(component, query);
-        },
+	// Return the setup object.
+	return {
+		provider,
+		world,
 
-        // Entities query function.
-        entities: async (component: string, partition: number) => {
-            return provider.entities(component, partition);
-        }
-    };
+		// Define contract components for the world.
+		contractComponents: defineContractComponents(world),
+
+		// Define the graph SDK instance.
+		graphSdk: () => getSdk(new GraphQLClient(VITE_PUBLIC_TORII)),
+
+		// Execute function.
+		execute: async (
+			signer: Account,
+			contract: string,
+			system: string,
+			call_data: num.BigNumberish[]
+		) => {
+			return provider.execute(signer, contract, system, call_data);
+		},
+
+		// Entity query function.
+		entity: async (model: string, query: Query) => {
+			return provider.entity(model, query);
+		},
+
+		// Entities query function.
+		entities: async (model: string, partition: number) => {
+			return provider.entities(model, partition);
+		},
+	};
 }
