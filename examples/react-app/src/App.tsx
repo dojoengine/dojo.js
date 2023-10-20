a275275a48ec5aeb2de78593f066270714d76130
democritus <ponderingdemocritus@protonmail.com>
2023-10-20 10:46:02 +0200

    client work

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -3,10 +3,12 @@ import { useDojo } from './DojoContext';
 import { useComponentValue } from "@latticexyz/react";
 import { Direction, } from './dojo/createSystemCalls'
 import { EntityIndex } from '@latticexyz/recs';
+import { useEffect, useMemo } from 'react';
 
 function App() {
   const {
     setup: {
+      network: { client },
       systemCalls: { spawn, move },
       components: { Moves, Position }
     },
@@ -19,6 +21,36 @@ function App() {
   // get current component values
   const position = useComponentValue(Position, entityId as EntityIndex);
   const moves = useComponentValue(Moves, entityId as EntityIndex);
+
+  useMemo(() => {
+    client.addEntitiesToSync([{
+      model: "Position",
+      keys: [
+        entityId,
+      ],
+    }])
+
+  }, []);
+
+  // useEffect(() => {
+  //   try {
+  //     client.addEntitiesToSync([{
+  //       model: "Position",
+  //       keys: [
+  //         entityId,
+  //       ],
+  //     }])
+  //   } catch (e) {
+  //     console.log("error adding entities to sync", e);
+  //   }
+
+  //   // const values = client.getModelValue("Position", [
+  //   //   "0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973",
+  //   // ]);
+  //   // console.log("Position changed", values);
+
+
+  // }, [position, moves]);
 
   return (
     <>
--- examples/react-app/src/dojo/setupNetwork.ts
+++ examples/react-app/src/dojo/setupNetwork.ts
@@ -9,6 +9,11 @@ import * as torii from "@dojoengine/torii";
 
 export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
 
+export interface ToriiModel {
+	model: string;
+	keys: string[];
+}
+
 export async function setupNetwork() {
 	// Extract environment variables for better readability.
 	const { VITE_PUBLIC_WORLD_ADDRESS, VITE_PUBLIC_NODE_URL, VITE_PUBLIC_TORII } =
@@ -22,32 +27,12 @@ export async function setupNetwork() {
 	);
 
 	const client = await torii.createClient(
-		[
-			{
-				model: "Position",
-				keys: [
-					"0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973",
-				],
-			},
-		],
+		[],
 		{
-			rpcUrl: "http://0.0.0.0:5050",
-			toriiUrl: "http://0.0.0.0:8080/grpc",
+			rpcUrl: VITE_PUBLIC_NODE_URL,
+			toriiUrl: VITE_PUBLIC_TORII + "/grpc",
 			worldAddress:
-				"0x1af130f7b9027f3748c1e3b10ca4a82ac836a30ac4f2f84025e83a99a922a0c",
-		}
-	);
-
-	client.onEntityChange(
-		{
-			model: "Position",
-			keys: ["0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973"],
-		},
-		() => {
-			const values = client.getModelValue("Position", [
-				"0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973",
-			]);
-			console.log("Position changed", values);
+				VITE_PUBLIC_WORLD_ADDRESS,
 		}
 	);
 
@@ -55,6 +40,18 @@ export async function setupNetwork() {
 	return {
 		provider,
 		world,
+		client,
+		worldAddress: VITE_PUBLIC_WORLD_ADDRESS,
+		// torii_client: async (models: ToriiModel[]) => {
+		// 	return await torii.createClient(models,
+		// 		{
+		// 			rpcUrl: VITE_PUBLIC_NODE_URL,
+		// 			toriiUrl: VITE_PUBLIC_TORII + "/grpc",
+		// 			worldAddress:
+		// 				VITE_PUBLIC_WORLD_ADDRESS,
+		// 		}
+		// 	);
+		// },
 
 		// Define contract components for the world.
 		contractComponents: defineContractComponents(world),
