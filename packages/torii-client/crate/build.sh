0cb31135051eb332547ac7798c6e90a30bfde841
Ammar Arif <ammr.arf@gmail.com>
2023-10-30 19:59:22 +0900

    `torii-client` fetch remote if entities not synced (#39)

--- packages/torii-client/crate/build.sh
+++ packages/torii-client/crate/build.sh
@@ -4,4 +4,4 @@ set -ex
 
 # This example requires to *not* create ES modules, therefore we pass the flag
 # `--target no-modules`
-wasm-pack build --out-dir ../wasm --release --features console-error-panic
+wasm-pack build --out-dir ../pkg --release --features console-error-panic
