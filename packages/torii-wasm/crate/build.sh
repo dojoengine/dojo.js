2de6f4c1202971d6962927fbadb2348c5e6a2963
R13 <rbentos@yahoo.com>
2023-11-27 17:50:16 -0300

    Removed wasm-pack global dependency on torii-wasm

--- packages/torii-wasm/crate/build.sh
+++ packages/torii-wasm/crate/build.sh
@@ -4,4 +4,4 @@ set -ex
 
 # This example requires to *not* create ES modules, therefore we pass the flag
 # `--target no-modules`
-wasm-pack build --out-dir ../pkg --release --features console-error-panic
+bunx wasm-pack build --out-dir ../pkg --release --features console-error-panic
