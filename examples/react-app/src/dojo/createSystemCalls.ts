6f794107de17fd4e03b6f4f2b8cdba7d92434945
7Stars <xiaochi@mono.fun>
2023-11-30 14:32:01 +0800

    Optimizing front-end display consistency in react-app

--- examples/react-app/src/dojo/createSystemCalls.ts
+++ examples/react-app/src/dojo/createSystemCalls.ts
@@ -26,7 +26,7 @@ export function createSystemCalls(
             entity: entityId,
             value: {
                 player: BigInt(entityId),
-                remaining: 10,
+                remaining: 100,
                 last_direction: 0,
             },
         });
