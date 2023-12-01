5d8b0e581a65bddc0b1df1dd4780b1e284484265
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-12-01 21:41:53 +0100

    0.4.0

--- packages/utils/src/utils/index.ts
+++ packages/utils/src/utils/index.ts
@@ -2,7 +2,6 @@ import { Event } from "starknet";
 import {
     Entity,
     setComponent,
-    Component,
     Components,
     ComponentValue,
     Type as RecsType,
@@ -72,10 +71,12 @@ export function setComponentFromEvent(
     const values = eventData.slice(index, index + numberOfValues);
 
     // create component object from values with schema
-    const componentValues = decodeComponent(component, [
-        ...string_keys,
-        ...values,
-    ]);
+    const valuesIndex = 0;
+    const componentValues = decodeComponent(
+        component.schema,
+        [...string_keys, ...values],
+        valuesIndex
+    );
 
     // console.log(componentName, entityIndex, componentValues);
 
@@ -111,39 +112,22 @@ export function parseComponentValue(value: string, type: RecsType) {
  * @returns {Object} The decoded component object.
  */
 export function decodeComponent(
-    component: Component,
+    schema: any,
     values: string[],
-    indices?: any
+    valuesIndex: number
 ): any {
-    const schema: any = component.schema;
-    const types: string[] = (component.metadata?.types as string[]) ?? [];
-    if (indices === undefined) {
-        indices = { types: 0, values: 0 };
-    }
-
     // Iterate through the keys of the schema and reduce them to build the decoded component.
     return Object.keys(schema).reduce((acc: any, key) => {
         // If the current schema key points to an object and doesn't have a 'type' property,
         // it means it's a nested component. Therefore, we recursively decode it.
         if (typeof schema[key] === "object" && !schema[key].type) {
-            acc[key] = decodeComponent(schema[key], values, indices);
+            acc[key] = decodeComponent(schema[key], values, valuesIndex);
         } else {
             // If the schema key points directly to a type or is not an object,
             // we parse its value using the provided parseComponentValue function
             // and move to the next index in the values array.
-            acc[key] = parseComponentValue(values[indices.values], schema[key]);
-            indices.values++;
-            // the u256 type in cairo is actually { low: u128, high: u128 }
-            // we need to consume two u128 values, shifting the second to compose u256
-            if (types[indices.types] == "u256") {
-                const value = parseComponentValue(
-                    values[indices.values],
-                    schema[key]
-                ) as bigint;
-                acc[key] |= value << 128n;
-                indices.values++;
-            }
-            indices.types++;
+            acc[key] = parseComponentValue(values[valuesIndex], schema[key]);
+            valuesIndex++;
         }
         return acc;
     }, {});
