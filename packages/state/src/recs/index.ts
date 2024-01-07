11ff3cbf5c7032bf65d35b77095b2b3707b6a02e
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-01-07 19:55:35 +1100

    add state package and refactor and remove sync from react

---
+++ packages/state/src/recs/index.ts
@@ -0,0 +1,73 @@
+import {
+    Component,
+    ComponentValue,
+    Entity,
+    Metadata,
+    Schema,
+    setComponent,
+} from "@dojoengine/recs";
+import { Client } from "@dojoengine/torii-client";
+import { convertValues } from "../utils";
+
+export const getSyncEntities = async <S extends Schema>(
+    client: Client,
+    components: Component<S, Metadata, undefined>[]
+) => {
+    await getEntities(client, components);
+    syncEntities(client, components);
+};
+
+export const getEntities = async <S extends Schema>(
+    client: Client,
+    components: Component<S, Metadata, undefined>[]
+) => {
+    let cursor = 0;
+    let continueFetching = true;
+
+    while (continueFetching) {
+        const entities = await client.getEntities(100, cursor);
+
+        setEntities(entities, components);
+
+        if (Object.keys(entities).length < 100) {
+            continueFetching = false;
+        } else {
+            cursor += 100;
+        }
+    }
+};
+
+export const syncEntities = async <S extends Schema>(
+    client: Client,
+    components: Component<S, Metadata, undefined>[]
+) => {
+    client.onEntityUpdated([], (entities: any) =>
+        setEntities(entities, components)
+    );
+};
+
+export const setEntities = async <S extends Schema>(
+    entities: any[],
+    components: Component<S, Metadata, undefined>[]
+) => {
+    for (let key in entities) {
+        if (entities.hasOwnProperty(key)) {
+            for (let componentName in entities[key]) {
+                if (entities[key].hasOwnProperty(componentName)) {
+                    let recsComponent = components[componentName as any];
+
+                    if (recsComponent) {
+                        setComponent(
+                            recsComponent,
+                            key as Entity,
+                            convertValues(
+                                recsComponent.schema,
+                                entities[key][componentName]
+                            ) as ComponentValue
+                        );
+                    }
+                }
+            }
+        }
+    }
+};
