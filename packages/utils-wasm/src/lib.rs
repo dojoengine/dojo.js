655129a20a7fd9e69ab305f061d0bfd6dba4d64e
julienbrs <julien.bourseau@gmail.com>
2024-01-12 19:07:49 +0100

    multiply function + tests multiply + floor

--- packages/utils-wasm/src/lib.rs
+++ packages/utils-wasm/src/lib.rs
@@ -1,9 +1,58 @@
 // JS bindings for simplex
 
 extern crate wasm_bindgen;
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub enum ScalarOrVec {
+    Scalar(f64),
+    Vec(Vec<f64>),
+}
+
+fn scalar_or_vec_to_js_value(scalar_or_vec: ScalarOrVec) -> JsValue {
+    match scalar_or_vec {
+        ScalarOrVec::Scalar(scalar) => JsValue::from_f64(scalar),
+        ScalarOrVec::Vec(vec) => {
+            serde_wasm_bindgen::to_value(&vec).unwrap() // todo: handle error
+        }
+    }
+}
+
+pub fn multiply_logic(a: ScalarOrVec, b: ScalarOrVec) -> ScalarOrVec {
+    use ScalarOrVec::*;
+
+    match (a, b) {
+        (Scalar(scalar_a), Scalar(scalar_b)) => Scalar(scalar_a * scalar_b),
+        (Scalar(scalar), Vec(vec)) | (Vec(vec), Scalar(scalar)) => {
+            Vec(vec.iter().map(|&x| x * scalar).collect())
+        }
+        (Vec(vec_a), Vec(vec_b)) => {
+            if vec_a.len() != vec_b.len() {
+                panic!("Vectors must be of the same length");
+            }
+            Vec(vec_a
+                .iter()
+                .zip(vec_b.iter())
+                .map(|(&x, &y)| x * y)
+                .collect())
+        }
+    }
+}
 
 #[wasm_bindgen]
-pub fn floor_vec(values: Vec<f64>) -> Vec<f64> {
+pub fn multiply(a: JsValue, b: JsValue) -> JsValue {
+    use multiply_logic;
+    let scalar_or_vec_a = serde_wasm_bindgen::from_value(a).unwrap(); // todo: handle error
+    let scalar_or_vec_b = serde_wasm_bindgen::from_value(b).unwrap();
+
+    let result = multiply_logic(scalar_or_vec_a, scalar_or_vec_b);
+
+    scalar_or_vec_to_js_value(result)
+}
+
+#[wasm_bindgen]
+pub fn floor(values: Vec<f64>) -> Vec<f64> {
     values.into_iter().map(|v| v.floor()).collect()
 }
---
+++ packages/utils-wasm/tests/lib_tests.rs
@@ -0,0 +1,43 @@
+extern crate wasm_bindgen;
+use utils_wasm::floor;
+use utils_wasm::multiply_logic;
+use utils_wasm::ScalarOrVec;
+
+
+/* Test multiply_logic */
+#[test]
+fn test_multiply_logic_scalars() {
+    let a = ScalarOrVec::Scalar(2.0);
+    let b = ScalarOrVec::Scalar(3.0);
+    assert_eq!(multiply_logic(a, b), ScalarOrVec::Scalar(6.0));
+}
+
+#[test]
+fn test_multiply_logic_scalar_vector() {
+    let a = ScalarOrVec::Scalar(2.0);
+    let b = ScalarOrVec::Vec(vec![1.0, 2.0, 3.0]);
+    assert_eq!(multiply_logic(a, b), ScalarOrVec::Vec(vec![2.0, 4.0, 6.0]));
+}
+
+#[test]
+fn test_multiply_logic_vector_scalar() {
+    let a = ScalarOrVec::Vec(vec![1.0, 2.0, 3.0]);
+    let b = ScalarOrVec::Scalar(2.0);
+    assert_eq!(multiply_logic(a, b), ScalarOrVec::Vec(vec![2.0, 4.0, 6.0]));
+}
+
+#[test]
+fn test_multiply_logic_vector_vector() {
+    let a = ScalarOrVec::Vec(vec![1.0, 2.0, 3.0]);
+    let b = ScalarOrVec::Vec(vec![2.0, 3.0, 4.0]);
+    assert_eq!(multiply_logic(a, b), ScalarOrVec::Vec(vec![2.0, 6.0, 12.0]));
+}
+
+/* Test floor */
+#[test]
+fn test_floor() {
+    let input = vec![0.1, 1.999, 4.5, 7.0];
+    let expected = vec![0.0, 1.0, 4.0, 7.0];
+    let actual = floor(input);
+    assert_eq!(expected, actual);
+}
