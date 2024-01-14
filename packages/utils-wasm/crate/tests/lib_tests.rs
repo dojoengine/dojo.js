d6065d1a203f4243db5bfb85bcfc9923782c73b5
julienbrs <julien.bourseau@gmail.com>
2024-01-14 13:46:48 +0100

    add tests to compare rust values to ts_values

--- packages/utils-wasm/crate/tests/lib_tests.rs
+++ packages/utils-wasm/crate/tests/lib_tests.rs
@@ -1,35 +1,6 @@
 extern crate utils_wasm;
 extern crate wasm_bindgen;
-use utils_wasm::types::ScalarOrVec;
-
-/* Test multiply_logic */
-// #[test]
-// fn test_multiply_logic_scalars() {
-//     let a = ScalarOrVec::Scalar(2.0);
-//     let b = ScalarOrVec::Scalar(3.0);
-//     assert_eq!(multiply_logic(a, b), ScalarOrVec::Scalar(6.0));
-// }
-
-// #[test]
-// fn test_multiply_logic_scalar_vector() {
-//     let a = ScalarOrVec::Scalar(2.0);
-//     let b = ScalarOrVec::Vec(vec![1.0, 2.0, 3.0]);
-//     assert_eq!(multiply_logic(a, b), ScalarOrVec::Vec(vec![2.0, 4.0, 6.0]));
-// }
-
-// #[test]
-// fn test_multiply_logic_vector_scalar() {
-//     let a = ScalarOrVec::Vec(vec![1.0, 2.0, 3.0]);
-//     let b = ScalarOrVec::Scalar(2.0);
-//     assert_eq!(multiply_logic(a, b), ScalarOrVec::Vec(vec![2.0, 4.0, 6.0]));
-// }
-
-// #[test]
-// fn test_multiply_logic_vector_vector() {
-//     let a = ScalarOrVec::Vec(vec![1.0, 2.0, 3.0]);
-//     let b = ScalarOrVec::Vec(vec![2.0, 3.0, 4.0]);
-//     assert_eq!(multiply_logic(a, b), ScalarOrVec::Vec(vec![2.0, 6.0, 12.0]));
-// }
+mod ts_values;
 
 /* Test floor */
 #[test]
@@ -74,5 +45,57 @@ fn test_taylor_inv_sqrt() {
     let result = utils_wasm::taylor_inv_sqrt(input);
     for (a, b) in result.iter().zip(expected.iter()) {
         assert!((a - b).abs() < 1e-9);
+    }
+}
+
+/* Test snoise */
+#[test]
+fn test_snoise() {
+    let mut results = Vec::new();
+    for x in 0..=9 {
+        for y in 0..=9 {
+            let input = vec![x as f64, 0.0, y as f64];
+            let noise_result = utils_wasm::snoise(&input);
+            results.push(noise_result);
+        }
+    }
+
+    for (result, expected) in results.iter().zip(ts_values::SNOISE_TS_VALUES.iter()) {
+        assert!((*result - expected).abs() < 1e-9);
+    }
+}
+
+/* Test recursive_s_noise */
+#[test]
+fn test_recursive_snoise() {
+    let mut results = Vec::new();
+    let pers_values = [0.5, 0.7];
+    let octave_counts = [1, 2, 3];
+
+    let mut points = Vec::new();
+
+    // Grid of points
+    for x in (-1..=1).map(|n| n as f64) {
+        for y in (-1..=1).map(|n| n as f64) {
+            for z in (-1..=1).map(|n| n as f64) {
+                points.push(vec![x, y, z]);
+            }
+        }
+    }
+
+    for p in &points {
+        for &pers in &pers_values {
+            for &octaves in &octave_counts {
+                let noise_value = utils_wasm::recursive_s_noise(p.to_vec(), pers, octaves);
+                results.push(noise_value);
+            }
+        }
+    }
+
+    for (result, expected) in results
+        .iter()
+        .zip(ts_values::RECURSIVE_SNOISE_TS_VALUES.iter())
+    {
+        assert!((*result - expected).abs() < 1e-9);
     }
 }
