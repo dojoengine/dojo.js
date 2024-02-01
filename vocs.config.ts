135a905acb64c620a5d20a09d2f184f1d00d4afd
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-02-02 10:19:49 +1100

    feat: docs init

---
+++ vocs.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+    description:
+        "Build reliable apps & libraries with lightweight, \
+    composable, and type-safe modules that interface with Ethereum.",
+    title: "Viem",
+    rootDir: "docs",
+});
