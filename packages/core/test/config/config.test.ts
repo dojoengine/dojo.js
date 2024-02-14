bef15cdb2113b1adb5236860d937f5485a904dbe
cpp-phoenix <gurjinders92571@gmail.com>
2024-02-14 17:18:28 +0530

    test: add core tests

---
+++ packages/core/test/config/config.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect, vi } from "vitest";
+import { createDojoConfig } from "../../src/config/index";
+
+describe("config", () => {
+    it("should return an object of type DojoConfigParams", async () => {
+        const configObj = createDojoConfig({
+            rpcUrl: "",
+            toriiUrl: "",
+            masterAddress: "",
+            masterPrivateKey: "",
+            accountClassHash: "",
+            manifest: "",
+        });
+        expect(configObj).toBeTypeOf("object");
+        expect(configObj.rpcUrl).not.toBeNull();
+        expect(configObj.toriiUrl).not.toBeNull();
+        expect(configObj.masterAddress).not.toBeNull();
+        expect(configObj.masterPrivateKey).not.toBeNull();
+        expect(configObj.accountClassHash).not.toBeNull();
+    });
+});
