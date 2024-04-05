7ae0c45ca4d5d6e9cd72ae5958270823effd25b0
Nasr <larko236@outlook.com>
2024-04-05 14:23:01 -0400

    feat(torii-wasm): encode typed data

--- packages/torii-wasm/crate/src/lib.rs
+++ packages/torii-wasm/crate/src/lib.rs
@@ -2,17 +2,19 @@
 
 use std::str::FromStr;
 
-use torii_grpc::types::{KeysClause, Clause, Query};
 use futures::StreamExt;
+use js_sys::JSON::stringify;
 use starknet::core::types::FieldElement;
 use starknet::core::utils::cairo_short_string_to_felt;
+use torii_grpc::types::{Clause, KeysClause, Query};
+use torii_relay::typed_data::TypedData;
 use wasm_bindgen::prelude::*;
 
 mod types;
 mod utils;
 
 use types::{ClientConfig, EntityModel, IEntityModel};
-use utils::{parse_ty_as_json_str, parse_entities_as_json_str};
+use utils::{parse_entities_as_json_str, parse_ty_as_json_str};
 
 type JsFieldElement = JsValue;
 
@@ -33,33 +35,34 @@ pub struct Client {
 #[wasm_bindgen]
 impl Client {
     #[wasm_bindgen(js_name = getEntities)]
-    pub async fn get_entities(
-        &self,
-        limit: u32,
-        offset: u32
-    ) -> Result<JsValue, JsValue> {
+    pub async fn get_entities(&self, limit: u32, offset: u32) -> Result<JsValue, JsValue> {
         #[cfg(feature = "console-error-panic")]
         console_error_panic_hook::set_once();
 
-        let results = self.inner.entities(Query {
-            clause: None,
-            limit,
-            offset
-        }).await;
+        let results = self
+            .inner
+            .entities(Query {
+                clause: None,
+                limit,
+                offset,
+            })
+            .await;
 
         match results {
-            Ok(entities) => Ok(js_sys::JSON::parse(&parse_entities_as_json_str(entities).to_string())?),
+            Ok(entities) => Ok(js_sys::JSON::parse(
+                &parse_entities_as_json_str(entities).to_string(),
+            )?),
             Err(err) => Err(JsValue::from(format!("failed to get entities: {err}"))),
         }
     }
-    
+
     #[wasm_bindgen(js_name = getEntitiesByKeys)]
     pub async fn get_entities_by_keys(
         &self,
         model: &str,
         keys: Vec<JsFieldElement>,
         limit: u32,
-        offset: u32
+        offset: u32,
     ) -> Result<JsValue, JsValue> {
         #[cfg(feature = "console-error-panic")]
         console_error_panic_hook::set_once();
@@ -70,17 +73,22 @@ impl Client {
             .collect::<Result<Vec<FieldElement>, _>>()
             .map_err(|err| JsValue::from(format!("failed to parse entity keys: {err}")))?;
 
-        let results = self.inner.entities(Query {
-            clause: Some(Clause::Keys(KeysClause {
-                model: model.to_string(),
-                keys
-            })),
-            limit,
-            offset
-        }).await;
-        
+        let results = self
+            .inner
+            .entities(Query {
+                clause: Some(Clause::Keys(KeysClause {
+                    model: model.to_string(),
+                    keys,
+                })),
+                limit,
+                offset,
+            })
+            .await;
+
         match results {
-            Ok(entities) => Ok(js_sys::JSON::parse(&parse_entities_as_json_str(entities).to_string())?),
+            Ok(entities) => Ok(js_sys::JSON::parse(
+                &parse_entities_as_json_str(entities).to_string(),
+            )?),
             Err(err) => Err(JsValue::from(format!("failed to get entities: {err}"))),
         }
     }
@@ -137,10 +145,7 @@ impl Client {
 
     /// Remove the entities from being synced.
     #[wasm_bindgen(js_name = removeModelsToSync)]
-    pub async fn remove_models_to_sync(
-        &self,
-        models: Vec<IEntityModel>,
-    ) -> Result<(), JsValue> {
+    pub async fn remove_models_to_sync(&self, models: Vec<IEntityModel>) -> Result<(), JsValue> {
         log("removing models to sync...");
 
         #[cfg(feature = "console-error-panic")]
@@ -196,28 +201,45 @@ impl Client {
         let ids = ids
             .unwrap_or(vec![])
             .into_iter()
-            .map(|id| FieldElement::from_str(&id).map_err(|err| JsValue::from(format!("failed to parse entity id: {err}"))))
+            .map(|id| {
+                FieldElement::from_str(&id)
+                    .map_err(|err| JsValue::from(format!("failed to parse entity id: {err}")))
+            })
             .collect::<Result<Vec<_>, _>>()?;
 
-        let mut stream = self
-            .inner
-            .on_entity_updated(ids)
-            .await
-            .unwrap();
+        let mut stream = self.inner.on_entity_updated(ids).await.unwrap();
 
         wasm_bindgen_futures::spawn_local(async move {
             while let Some(update) = stream.next().await {
                 let entity = update.expect("no updated entity");
                 let json_str = parse_entities_as_json_str(vec![entity]).to_string();
                 let _ = callback.call1(
-                    &JsValue::null(), 
-                    &js_sys::JSON::parse(&json_str).expect("json parse failed")
+                    &JsValue::null(),
+                    &js_sys::JSON::parse(&json_str).expect("json parse failed"),
                 );
             }
         });
 
         Ok(())
     }
+}
+
+#[wasm_bindgen(js_name = encoreTypedData)]
+#[allow(non_snake_case)]
+pub fn encode_typed_data(data: JsValue, address: &str) -> Result<String, JsValue> {
+    let data: String =
+        stringify(&data).map_err(|_| JsValue::from(format!("failed to stringify typed data")))?.into();
+
+    let typed_data = serde_json::from_str::<TypedData>(&data)
+        .map_err(|err| JsValue::from(format!("failed to parse typed data: {err}")))?;
+
+    let address = FieldElement::from_str(address)
+        .map_err(|err| JsValue::from(format!("failed to parse address: {err}")))?;
+
+    typed_data
+        .encode(address)
+        .map(|f| format!("{:#x}", f))
+        .map_err(|err| JsValue::from(format!("failed to encode typed data: {err}")))
 }
 
 /// Create the a client with the given configurations.
@@ -245,16 +267,26 @@ pub async fn create_client(
     let world_address = FieldElement::from_str(&world_address)
         .map_err(|err| JsValue::from(format!("failed to parse world address: {err}")))?;
 
-    let client =
-        torii_client::client::Client::new(torii_url, rpc_url, relay_url, world_address, Some(models))
-            .await
-            .map_err(|err| JsValue::from(format!("failed to build client: {err}")))?;
+    let client = torii_client::client::Client::new(
+        torii_url,
+        rpc_url,
+        relay_url,
+        world_address,
+        Some(models),
+    )
+    .await
+    .map_err(|err| JsValue::from(format!("failed to build client: {err}")))?;
 
     wasm_bindgen_futures::spawn_local(client.start_subscription().await.map_err(|err| {
         JsValue::from(format!(
             "failed to start torii client subscription service: {err}"
         ))
     })?);
+
+    let relay_runner = client.relay_runner();
+    wasm_bindgen_futures::spawn_local(async move {
+        relay_runner.lock().await.run().await;
+    });
 
     Ok(Client { inner: client })
 }
