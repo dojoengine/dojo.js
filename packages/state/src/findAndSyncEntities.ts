9b9ff00199f40ef4e328b6ac8010fc3c2dac1e39
Jasper Dansercoer <jasper@codictive.be>
2024-04-08 14:58:53 +0200

    feat: added singular findAndSyncEntity

--- packages/state/src/findAndSyncEntities.ts
+++ packages/state/src/findAndSyncEntities.ts
@@ -22,11 +22,48 @@ export const findAndSyncEntities = async <
 
     dataStuff.torii.onEntityUpdated(
         idsToWatch,
-        (entity: Awaited<ReturnType<T["findEntities"]>>) => {
-            store.setState({ ...entity });
-            callback?.(entity);
+        (entities: Awaited<ReturnType<T["findEntities"]>>) => {
+            store.setState({ ...entities });
+            callback?.(entities);
         }
     );
 
     return result;
+};
+
+export const findAndSyncEntity = async <
+    T extends {
+        torii: Client;
+        findEntities: () => Promise<Record<string, any>>;
+    },
+>(
+    data: Promise<T>,
+    callback?: (
+        entity: T extends { findEntities: () => Promise<infer R> }
+            ? R[keyof R]
+            : any
+    ) => void
+): Promise<
+    T extends { findEntities: () => Promise<infer R> } ? R[keyof R] : any
+> => {
+    const dataStuff = await data;
+
+    const result = await dataStuff.findEntities();
+
+    const [firstKey, firstEntity] = Object.entries(result)[0] as [string, any];
+
+    store.setState({ [firstKey]: firstEntity });
+
+    dataStuff.torii.onEntityUpdated(
+        [firstKey],
+        (entities: Record<string, any>) => {
+            const [firstUpdatedKey, firstUpdatedEntity] = Object.entries(
+                entities
+            )[0] as [string, any];
+            store.setState({ [firstUpdatedKey]: firstUpdatedEntity });
+            callback?.(firstUpdatedEntity);
+        }
+    );
+
+    return firstEntity;
 };
