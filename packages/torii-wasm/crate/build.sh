446c607907a40bf483b0efc362072052533f13e9
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-06-14 12:04:03 +1000

    fix: fix build

--- packages/torii-wasm/crate/build.sh
+++ packages/torii-wasm/crate/build.sh
@@ -4,4 +4,4 @@ set -ex
 
 # This example requires to *not* create ES modules, therefore we pass the flag
 # `--target no-modules`
-npx wasm-pack build --out-dir ../pkg --release  --target no-modules
+npx wasm-pack build --out-dir ../pkg --release
