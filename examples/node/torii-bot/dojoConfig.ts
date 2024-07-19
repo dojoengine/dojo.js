f3da58a42afefe3be96c653826d51f72a1498f79
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-07-19 12:16:27 +1000

    bump examples

--- examples/node/torii-bot/dojoConfig.ts
+++ examples/node/torii-bot/dojoConfig.ts
@@ -1,4 +1,4 @@
-import manifest from "../../dojo-starter/manifests/dev/manifest.json" assert { type: "json" };
+import manifest from "../../dojo-starter/manifests/dev/deployment/manifest.json" assert { type: "json" };
 
 import { createDojoConfig } from "@dojoengine/core";
 
