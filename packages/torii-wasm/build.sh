fae3cacee11f1fbf5df156c53dde87934c943392
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-08-17 09:40:58 -0400

    fix: wasm build issues

--- packages/torii-wasm/build.sh
+++ packages/torii-wasm/build.sh
@@ -8,14 +8,14 @@ git clone --depth 1 https://github.com/dojoengine/dojo.c dojo.c
 cd dojo.c
 
 # Build for web (browser)
-npx wasm-pack build --out-dir ../dist/pkg/web --release --target web
+npx wasm-pack build --out-dir ../pkg/web --release
 
 # Build for Node.js
-npx wasm-pack build --out-dir ../dist/pkg/node --release --target nodejs
+npx wasm-pack build --out-dir ../pkg/node --release --target nodejs
 
 # Find and delete .gitignore files in the specified directories
-find ../dist/pkg/node -name ".gitignore" -type f -delete
-find ../dist/pkg/web -name ".gitignore" -type f -delete
+find ../pkg/node -name ".gitignore" -type f -delete
+find ../pkg/web -name ".gitignore" -type f -delete
 
 
 # Go back to the parent directory and delete the repository
