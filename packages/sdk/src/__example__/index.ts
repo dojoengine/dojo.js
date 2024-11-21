e7715f6bc503d2e0d90e08c5878028066fbb41f7
Valentin Dosimont <valentin@pupucecorp.com>
2024-11-21 18:47:13 +0100

    feat: add examples

--- packages/sdk/src/__example__/index.ts
+++ packages/sdk/src/__example__/index.ts
@@ -1,7 +1,7 @@
 // EXAMPLE FOR NOW
 
 import * as torii from "@dojoengine/torii-client";
-import { init } from "..";
+import { init, QueryBuilder } from "..";
 import { SchemaType } from "../types";
 
 export interface PlayerModel {
@@ -111,73 +111,49 @@ async function exampleUsage() {
         invalidMessage // TypeScript Error
     );
 
-    db.subscribeEntityQuery(
-        {
-            world: {
-                player: {
-                    $: {
-                        where: {
-                            name: { $is: "Alice" },
-                            score: { $is: 10 },
-                        },
-                    },
-                },
-            },
-        },
-        (resp) => {
+    const query = new QueryBuilder<MockSchemaType>().namespace("world", (n) =>
+        n.entity("player", (e) => e.eq("name", "Alice"))
+    );
+
+    db.subscribeEntityQuery(query.build(), (resp) => {
+        if (resp.error) {
+            console.error(
+                "Error querying todos and goals:",
+                resp.error.message
+            );
+            return;
+        }
+        if (resp.data) {
+            console.log(
+                "Queried todos and goals:",
+                resp.data.map((a) => a.models.world)
+            );
+        }
+    });
+    // Example usage of getEntities with where clause
+    try {
+        const eq = new QueryBuilder<MockSchemaType>().namespace("world", (n) =>
+            n
+                .entity("item", (e) =>
+                    e.eq("type", "sword").lt("durability", 5)
+                )
+                .entity("game", (e) => e.eq("status", "completed"))
+        );
+        const entities = await db.getEntities(eq.build(), (resp) => {
             if (resp.error) {
                 console.error(
-                    "Error querying todos and goals:",
+                    "Error querying completed important todos:",
                     resp.error.message
                 );
                 return;
             }
             if (resp.data) {
                 console.log(
-                    "Queried todos and goals:",
-                    resp.data.map((a) => a.models.world)
+                    "Completed important todos:",
+                    resp.data.map((a) => a.models)
                 );
             }
-        }
-    );
-    // Example usage of getEntities with where clause
-    try {
-        const entities = await db.getEntities(
-            {
-                world: {
-                    item: {
-                        $: {
-                            where: {
-                                type: { $eq: "sword" },
-                                durability: { $lt: 5 },
-                            },
-                        },
-                    },
-                    game: {
-                        $: {
-                            where: {
-                                status: { $eq: "completed" },
-                            },
-                        },
-                    },
-                },
-            },
-            (resp) => {
-                if (resp.error) {
-                    console.error(
-                        "Error querying completed important todos:",
-                        resp.error.message
-                    );
-                    return;
-                }
-                if (resp.data) {
-                    console.log(
-                        "Completed important todos:",
-                        resp.data.map((a) => a.models)
-                    );
-                }
-            }
-        );
+        });
         console.log("Queried entities:", entities);
     } catch (error) {
         console.error("Error querying entities:", error);
