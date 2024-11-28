4950ca4cd40f89a9b2efa25e5e86d3f67684bdb4
Tony Stark <oktonystark8@gmail.com>
2024-11-28 02:41:43 -0600

    fix: renamed file path to contracts

--- packages/create-dojo/src/commands/start.ts
+++ packages/create-dojo/src/commands/start.ts
@@ -44,7 +44,7 @@ const templates = [
 async function init(projectName: string, cwd: string, template: string) {
     const projectPath = path.join(cwd, projectName);
     const clientPath = path.join(projectPath, "client");
-    const contractPath = path.join(projectPath, "contract");
+    const contractPath = path.join(projectPath, "contracts");
 
     // Create project directories
     await fs.mkdir(projectPath, { recursive: true });
