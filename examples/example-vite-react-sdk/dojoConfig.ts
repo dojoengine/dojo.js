6a3f210df5ba11287a57ea73f57c3b07e38b034c
Tony Stark <oktonystark8@gmail.com>
2024-11-28 02:06:18 -0600

    feat: fixed issue with example-vite-react-sdk

--- examples/example-vite-react-sdk/dojoConfig.ts
+++ examples/example-vite-react-sdk/dojoConfig.ts
@@ -4,4 +4,10 @@ import manifest from "../../worlds/dojo-starter/manifest_dev.json";
 
 export const dojoConfig = createDojoConfig({
     manifest,
+    rpcUrl: "http://localhost:5050/",
+    toriiUrl: "http://localhost:8080",
+    masterAddress:
+        "0x127fd5f1fe78a71f8bcd1fec63e3fe2f0486b6ecd5c86a0466c3a21fa5cfcec",
+    masterPrivateKey:
+        "0xc5b2fcab997346f3ea1c00b002ecf6f382c5f9c9659a3894eb783c5320f912",
 });
