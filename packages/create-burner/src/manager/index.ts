eca9a11f6383348333e2e0d0e735059af4f21110
Valentin Dosimont <valentin@pupucecorp.com>
2025-08-05 14:30:58 +0200

    chore: update starknet.js version

--- packages/create-burner/src/manager/index.ts
+++ packages/create-burner/src/manager/index.ts
@@ -1,3 +1,25 @@
+import {
+    Account,
+    CairoVersion,
+    ProviderInterface,
+    ProviderOptions,
+    SignerInterface,
+} from "starknet";
+
+export function newAccount(
+    provider: ProviderOptions | ProviderInterface,
+    address: string,
+    signer: Uint8Array | string | SignerInterface,
+    cairoVersion: CairoVersion
+): Account {
+    return new Account({
+        provider,
+        address,
+        signer,
+        cairoVersion,
+    });
+}
+
 export { BurnerManager } from "./burnerManager";
 export { PredeployedManager } from "./predeployedManager";
 export { prefundAccount } from "./prefundAccount";
